798912cf3f31a5b87f78efadd65c8016d29ac618
Samuel Gaus <sam@gaus.co.uk>
2017-08-03 12:44:13 +0100

    Make AMD setup compatible with case sensitive filesystems (#140)

--- src/js/.wrapper.js
+++ src/js/.wrapper.js
@@ -1,9 +1,9 @@
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
-    define(['three', 'D.js', 'uevent', 'doT'], factory);
+    define(['three', 'd.js', 'uevent', 'dot/doT'], factory);
   }
   else if (typeof module === 'object' && module.exports) {
-    module.exports = factory(require('three'), require('d.js'), require('uevent'), require('dot'));
+    module.exports = factory(require('three'), require('d.js'), require('uevent'), require('dot/doT'));
   }
   else {
     root.PhotoSphereViewer = factory(root.THREE, root.D, root.uEvent, root.doT);
