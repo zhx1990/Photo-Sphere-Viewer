b51c3a83a0c741d8e72e300c0f492277129cbd57
mistic100 <contact@git.strangeplanet.fr>
2020-08-22 11:43:27 +0200

    Handle API doc links

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -2,7 +2,16 @@ import { MdField, MdButton } from "vue-material/dist/components";
 import 'vue-material/dist/theme/default.css'
 import 'vue-material/dist/vue-material.min.css'
 
-export default ({ Vue }) => {
+export default ({ Vue, router }) => {
   Vue.use(MdField);
   Vue.use(MdButton);
+
+  router.beforeEach((to, from, next) => {
+    if (/^\/api/.test(to.fullPath)) {
+      window.location.href  = `${to.path}.html${to.hash}`;
+      next(false);
+    } else {
+      next();
+    }
+  });
 };
