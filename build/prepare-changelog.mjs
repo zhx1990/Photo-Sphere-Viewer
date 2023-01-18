5180dbdf790864f315e572c791d3981b5bde772b
Damien SOREL <damien.sorel@sogelink.fr>
2023-01-18 14:04:17 +0100

    chore: auto prepare release note

---
+++ build/prepare-changelog.mjs
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+/**
+ * Generate the release note
+ */
+
+import fs from 'fs';
+import { stdin } from 'process';
+
+const FROM_TAG = process.argv[2];
+const TO_TAG = process.argv[3];
+
+const FILENAME = `dist/changelog_${TO_TAG}.md`;
+
+if (!FROM_TAG || !TO_TAG || FROM_TAG === TO_TAG) {
+    console.warn('No tags provided or same tags');
+    writeFileAndExit('');
+}
+
+let log = '';
+
+stdin.setEncoding('utf8');
+
+stdin.on('data', (chunk) => {
+    log += chunk;
+});
+
+stdin.on('error', (e) => {
+    console.error(e);
+    writeFileAndExit('');
+});
+
+stdin.on('end', () => {
+    const content = `[Full changelog](https://github.com/mistic100/Photo-Sphere-Viewer/compare/${FROM_TAG}...${TO_TAG})
+
+${log.trim()
+    .split('\n')
+    .map((line) => line.trim())
+    .filter((line) => !line.startsWith('chore'))
+    .map((line) => ` - ${line}`)
+    .join('\n')
+}`;
+
+    writeFileAndExit(content);
+});
+
+function writeFileAndExit(content) {
+    fs.writeFileSync(FILENAME, content);
+    process.exit(0);
+}
