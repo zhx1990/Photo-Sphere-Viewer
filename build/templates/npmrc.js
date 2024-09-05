f2d409f3a84bccc21e1ae004268ed436ae35b115
{haoxian1990} <149322439@qq.com>
2024-09-05 10:50:24 +0800

    updae

--- build/templates/npmrc.js
+++ build/templates/npmrc.js
@@ -1,8 +1,13 @@
+// export const npmrc = () =>
+//     Promise.resolve(`
+// home=https://npmmirror.com
+// registry=https://packages.aliyun.com/60d1e8ad077c732675e5fbb8/npm/npm-registry/
+// //registry.npmjs.org/:_authToken=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
+// //packages.aliyun.com/60d1e8ad077c732675e5fbb8/npm/npm-registry/:_authToken=332684b4-a8ae-40dc-bc6f-1f5600547f4d
+// always-auth=true
+// `);
+
 export const npmrc = () =>
-    Promise.resolve(`
-home=https://npmmirror.com
-registry=https://packages.aliyun.com/60d1e8ad077c732675e5fbb8/npm/npm-registry/
-//registry.npmjs.org/:_authToken=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
-//packages.aliyun.com/60d1e8ad077c732675e5fbb8/npm/npm-registry/:_authToken=332684b4-a8ae-40dc-bc6f-1f5600547f4d
-always-auth=true
+    Promise.resolve(`@photo-sphere-viewer:registry=https://registry.npmjs.org
+//registry.npmjs.org/:_authToken=\${NODE_AUTH_TOKEN}
 `);
