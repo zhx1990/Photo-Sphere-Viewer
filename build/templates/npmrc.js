b97b2de603d2ab892e8302addb28bf10e87f695a
{haoxian1990} <149322439@qq.com>
2024-09-05 10:48:06 +0800

    更新配置文件

--- build/templates/npmrc.js
+++ build/templates/npmrc.js
@@ -2,7 +2,7 @@ export const npmrc = () =>
     Promise.resolve(`
 home=https://npmmirror.com
 registry=https://packages.aliyun.com/60d1e8ad077c732675e5fbb8/npm/npm-registry/
-//registry.npmjs.org/:_authToken=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
+//registry.npmjs.org/:_authToken=\${NODE_AUTH_TOKEN}
 //packages.aliyun.com/60d1e8ad077c732675e5fbb8/npm/npm-registry/:_authToken=332684b4-a8ae-40dc-bc6f-1f5600547f4d
 always-auth=true
 `);
